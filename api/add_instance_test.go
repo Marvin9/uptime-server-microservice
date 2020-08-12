98bfdc8c9ef2741682ca70a783520d752702343d
Marvin9 <marvinduff97@gmail.com>
2020-08-12 19:03:15 +0530

    test: Added 'Add instance' test & simulate APIs with cookies too

---
+++ api/add_instance_test.go
@@ -0,0 +1,88 @@
+package api_test
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/Marvin9/uptime-server-microservice/api/middlewares"
+	"github.com/Marvin9/uptime-server-microservice/api/setup"
+	"github.com/Marvin9/uptime-server-microservice/pkg/database"
+	"github.com/Marvin9/uptime-server-microservice/pkg/models"
+	"github.com/Marvin9/uptime-server-microservice/test"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+const addInstanceAPI = "/api/instance"
+
+type responseData struct {
+	Data string `json:"data"`
+}
+
+func TestAddInstanceAPI(t *testing.T) {
+	test.FakeDB(test.CREATE)
+	defer test.FakeDB(test.DROP)
+
+	db, err := database.ConnectDB()
+	if err != nil {
+		t.Errorf("Error connecting database.\n%v\n", err)
+	}
+	defer db.Close()
+	db.AutoMigrate(&models.Users{}, &models.Instances{}, &models.Reports{})
+
+	credentials := []byte(`{ "email": "mayursiinh@gmail.com", "password": "abc" }`)
+	register := test.SimulationData{
+		Method:             "POST",
+		API:                "/auth/register",
+		Body:               credentials,
+		ExpectedStatusCode: http.StatusOK,
+		ErrorMessage:       "Error while registering.",
+	}
+
+	router := setup.Router()
+	test.SimulateAPI(t, router, register)
+
+	_, jwtToken, err := database.LoginUser("mayursiinh@gmail.com", "abc")
+	if err != nil {
+		t.Errorf("Error logging in.\n%v", err)
+	}
+	cookie := http.Cookie{
+		Name:  middlewares.JWTCookieName,
+		Value: jwtToken,
+	}
+
+	// 1 ^ 9 = 1 second
+	instanceBody := []byte(`{ "url": "https://www.google.com", "duration": 1000000000 }`)
+	addInstance := test.SimulationData{
+		Method:             "POST",
+		API:                addInstanceAPI,
+		Body:               instanceBody,
+		ExpectedStatusCode: http.StatusBadRequest,
+		ErrorMessage:       "Duration of instance should be minimum 1 hour, but it accepted less than that",
+		Cookie:             cookie,
+	}
+	test.SimulateAPI(t, router, addInstance)
+
+	// 1 hour
+	instanceBody = []byte(`{ "url": "https://www.google.com", "duration": 3600000000000 }`)
+	addInstance.Body = instanceBody
+	addInstance.ExpectedStatusCode = http.StatusOK
+	addInstance.ErrorMessage = "Instance was not added."
+
+	resp := test.SimulateAPI(t, router, addInstance)
+	var storeResponseData responseData
+	buf, _ := ioutil.ReadAll(resp.Body)
+	json.Unmarshal([]byte(buf), &storeResponseData)
+	newInstanceID := storeResponseData.Data
+
+	var instanceData models.Instances
+	addedInstanceNotFound := db.Where("unique_id = ?", newInstanceID).First(&instanceData).RecordNotFound()
+	if addedInstanceNotFound {
+		t.Errorf("Instance %v was not stored in database.\n", newInstanceID)
+	}
+
+	if !database.IsInstanceRunning(instanceData.Owner, instanceData.URL) {
+		t.Errorf("Instance was created but not running in memory.\n")
+	}
+}
--- test/simulate_api.go
+++ test/simulate_api.go
@@ -16,6 +16,7 @@ type SimulationData struct {
 	Body               []byte
 	ExpectedStatusCode int
 	ErrorMessage       string
+	Cookie             http.Cookie
 }
 
 // SetRequestHeaderJSON - as name suggests
@@ -29,14 +30,18 @@ func ResponseError(t *testing.T, message string, expected, got int, body *bytes.
 }
 
 // SimulateAPI - simulate API tests
-func SimulateAPI(t *testing.T, router *gin.Engine, s SimulationData) {
+func SimulateAPI(t *testing.T, router *gin.Engine, s SimulationData) *httptest.ResponseRecorder {
 	w := httptest.NewRecorder()
+	http.SetCookie(w, &s.Cookie)
 	jsonBody := bytes.NewBuffer(s.Body)
 	req, _ := http.NewRequest(s.Method, s.API, jsonBody)
+	req.Header = http.Header{"Cookie": w.HeaderMap["Set-Cookie"]}
 	SetRequestHeaderJSON(req)
 	router.ServeHTTP(w, req)
 
 	if w.Code != s.ExpectedStatusCode {
 		ResponseError(t, s.ErrorMessage, s.ExpectedStatusCode, w.Code, w.Body)
 	}
+
+	return w
 }
