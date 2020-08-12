bc3055adf2fce4c95d231422d2cc7720b377a54e
Marvin9 <marvinduff97@gmail.com>
2020-08-12 09:49:56 +0530

    refactor

--- test/fake_db.go
+++ test/fake_db.go
@@ -11,13 +11,18 @@ import (
 )
 
 const (
+	// CREATE - operation to create database
 	CREATE = "CREATE"
-	DROP   = "DROP"
+	// DROP - operation to drop database
+	DROP = "DROP"
 )
 
+// FakeDB is used in unit tests to mock db
 func FakeDB(operation string) {
 	godotenv.Load("../../.env")
 	dbname := os.Getenv("DATABASE_NAME")
+	postgreUser := os.Getenv("PSQL_USER")
+	postgrePassword := os.Getenv("PSQL_PASSWORD")
 	var message string
 	var command string
 	if operation == CREATE {
@@ -28,9 +33,9 @@ func FakeDB(operation string) {
 		command = "dropdb"
 	}
 
-	cmd := exec.Command(command, "-h", "localhost", "-U", os.Getenv("PSQL_USER"), "-e", dbname)
+	cmd := exec.Command(command, "-h", "localhost", "-U", postgreUser, "-e", dbname)
 	cmd.Env = os.Environ()
-	cmd.Env = append(cmd.Env, fmt.Sprintf("PGPASSWORD=%v", os.Getenv("PSQL_PASSWORD")))
+	cmd.Env = append(cmd.Env, fmt.Sprintf("PGPASSWORD=%v", postgrePassword))
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	if err := cmd.Run(); err != nil {
