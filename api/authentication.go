07bad257fa5c4a3ebef0801e5844409e51fa8c71
Marvin9 <marvinduff97@gmail.com>
2020-08-17 09:01:09 +0530

    fix: SameSite none mode + secure = true, chrome only accept those cookies,

--- api/authentication.go
+++ api/authentication.go
@@ -50,8 +50,8 @@ func LoginAPI(c *gin.Context) {
 
 	log.Printf("\n\nCookie expiration time: %v", int(utils.JWTCookieExpireAfter))
 
-	// c.SetSameSite(http.SameSiteNoneMode)
-	c.SetCookie(middlewares.JWTCookieName, jwtToken, int(utils.JWTCookieExpireAfter), "/", "localhost", false, true)
+	c.SetSameSite(4)
+	c.SetCookie(middlewares.JWTCookieName, jwtToken, int(utils.JWTCookieExpireAfter), "/", "localhost", true, true)
 	c.JSON(statusCode, models.SuccessResponse("Successfully logged in."))
 }
 
